7940af42c8bb20d0e88530985da045e28c764660
Valentin <valentincanale@gmail.com>
2020-08-25 17:47:26 -0300

    subida js

---
+++ js/index.js
@@ -0,0 +1,54 @@
+window.sr = ScrollReveal ();
+
+
+	sr.reveal ('.navbar', {
+		duration: 2000,
+		origin: 'bottom'
+
+	});
+
+	sr.reveal ('.header-content-left', {
+		duration: 2000,
+		origin: 'left',
+		distance: '300px'
+
+	});
+
+	sr.reveal ('.header-content-right', {
+		duration: 2000,
+		origin: 'right',
+		distance: '300px'
+
+	});
+
+
+	sr.reveal ('.header-btn ', {
+		duration: 2000,
+		origin: 'bottom',
+		delay: 1000
+
+	});
+
+	sr.reveal ('#body ', {
+		duration: 2000,
+		origin: 'left',
+		distance: '300px'
+
+	});
+
+	sr.reveal ('#modulo1 ', {
+		duration: 2000,
+		origin: 'right',
+		distance: '300px'
+		
+	});
+
+	sr.reveal ('#modulo2 ', {
+		duration: 2000,
+		origin: 'left',
+		distance: '300px'
+		
+	});
+
+	
+
